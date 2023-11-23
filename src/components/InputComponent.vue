42eb2d136f231b4006fc58917f790242be5fdc67
Matias198 <matias.fernandez099@gmail.com>
2023-11-23 15:18:46 -0300

    firebase+vue

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <fwb-input
+      v-model="name"
+      placeholder="enter your first name"
+      label="First name"
+    />
+  </template>
+  
+  <script lang="ts" setup>
+  import { ref } from 'vue'
+  import { FwbInput } from 'flowbite-vue'
+  
+  const name = ref('')
+  </script>
